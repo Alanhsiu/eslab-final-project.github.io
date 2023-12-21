ae5d18ac5b59b45e2ce44f6bc014cd441fcd25b2
蔡名峴 <minghowthz@minghowthz.local>
2023-12-22 02:45:09 +0800

    2:44pm test

--- websocket/canva.py
+++ websocket/canva.py
@@ -12,7 +12,7 @@ class basketball():
     boardposition = 1.2
     
     backboard = box(pos=vector(0, boardheight+heightadjust, z_boarder+boardposition), size=vector(1.8, 1.2, 0.1), color=color.white)
-    hoop = ring(pos=vector(0, hoopheight+heightadjust, backboard.pos.z+d_fromboard), axis=vector(0, 0.01, 0), radius = 0.23, thickness=0.03, color=color.red)
+    hoop = ring(pos=vector(0, hoopheight+heightadjust, backboard.pos.z+d_fromboard), axis=vector(0, 0.01, 0), radius = 0.4, thickness=0.03, color=color.red)
     hoop.pos.z += hoop.radius
     
     # generate the connecting rod
@@ -43,23 +43,27 @@ class basketball():
     for angle in range(0, 180):  # 繪製半圓形的三分線
         x = 7.24 * cos(radians(angle))
         y = heightadjust
-        z = 2 + 7.24 * sin(radians(angle)) + z_boarder
+        z = 7.24 * sin(radians(angle)) + hoop.pos.z
         three_point_line.append(vector(x, y, z))
     
     # generate the basketball
     shootheight = 1.9
     ball_radius = 0.12
     basketball = sphere(pos=vector(0, shootheight+heightadjust,  freethrowline.pos.z), radius=ball_radius, color=color.orange, make_trail=False, trail_color=color.white)
-    
-    def set_shoot_position(self, level): # level 0: 2-point shot, level 1: 3-point shot
-        dist = self.dist_2_point if level == 0 else self.dist_3_point
+    level = 0
+
+    def set_shoot_position(self, assignlevel): # level 0: 2-point shot, level 1: 3-point shot
+        dist = self.dist_2_point if assignlevel == 0 else self.dist_3_point
+        self.level = assignlevel
         self.basketball.pos = vector(0, self.shootheight+self.heightadjust, dist-self.ball_radius)
-    
+        
     def shoot(self, ax = 0, ay = 0, az = 0):
         # initial position of the ball
-        self.set_shoot_position(level=0)
-        self.basketball.velocity = vector(0, 7, -3)
-        self.basketball.velocity += vector(ay*0.001, az*(-0.002), ax*(0.01))
+        
+        if self.level == 0 : self.basketball.velocity = vector(0, 8, -3)
+        else : self.basketball.velocity = vector(0, 9, -4)
+        
+        self.basketball.velocity += vector(ay*0.0005, az*(-0.0005), ax*(0.0005))
         g = vector(0, -9.8, 0)
 
         dt = 0.001  # time interval
@@ -71,7 +75,7 @@ class basketball():
             time += dt
             if time > maxtime:
                 time = 0
-                self.basketball.pos = vector(0, self.shootheight+self.heightadjust, self.paintwidth-self.ball_radius)
+                self.set_shoot_position(self.level)
                 return False
             
             rate(800)  # 1000 frames per second
@@ -90,6 +94,8 @@ class basketball():
             if(self.basketball.pos.y <= self.hoop.pos.y+0.1 and (self.basketball.pos.y >= self.hoop.pos.y-0.1) and mag(vector(self.basketball.pos.x, 0, self.basketball.pos.z)-vector(self.hoop.pos.x, 0, self.hoop.pos.z)) <= self.hoop.radius-self.basketball.radius-self.hoop.thickness) and (self.basketball.velocity.y <=0):
                 # TODO: display "2-point shot" or "3-point shot" on the canvas
                 time = 0
+                self.set_shoot_position(self.level)
+                print("2-point shot made")
                 return True
             
             self.basketball.velocity += g * dt
