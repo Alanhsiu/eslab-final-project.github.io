908a9b42b9e904c567080aea0ad63569dd6edc51
蔡名峴 <minghowthz@minghowthz.local>
2023-12-22 00:30:33 +0800

    parameterdefine

--- websocket/canva.py
+++ websocket/canva.py
@@ -2,34 +2,47 @@ from vpython import *
 
 class basketball():
     scene = canvas(title='3D Basketball Court', width=1200, height=600)
-    player_height = 1.9
     heightadjust = -3
     court = box(pos=vector(0, heightadjust, 0), size=vector(15, 0.1, 14), color=color.white)
-    center = -court.size.z/2
-    backboard = box(pos=vector(0, 3.5+heightadjust, center +1.2), size=vector(1.8, 1.2, 0.1), color=color.white)
-    hoop = ring(pos=vector(0, 3.05+heightadjust, backboard.pos.z+0.23), axis=vector(0, 0.01, 0), radius = 0.23, thickness=0.03, color=color.red)
+    z_boarder = -court.size.z/2
+    
+    boardheight = 3.5
+    hoopheight = 3.05
+    d_fromboard = 0.23
+    boardposition = 1.2
+    
+    backboard = box(pos=vector(0, boardheight+heightadjust, z_boarder+boardposition), size=vector(1.8, 1.2, 0.1), color=color.white)
+    hoop = ring(pos=vector(0, hoopheight+heightadjust, backboard.pos.z+d_fromboard), axis=vector(0, 0.01, 0), radius = 0.23, thickness=0.03, color=color.red)
     hoop.pos.z += hoop.radius
     
-    # generate the paint area
-    paint1 = box(pos = vector(4.9/2-0.05, heightadjust,center+5.8/2), size = vector(0.1, 0.1, 5.8), color = color.red, opacity = 1)
-    paint2 = box(pos = vector(-4.9/2+0.05, heightadjust,center+5.8/2), size = vector(0.1, 0.1, 5.8), color = color.red, opacity = 1)
-    paint3 = box(pos = vector(0, heightadjust,5.8/2-0.05+center+5.8/2), size = vector(4.9, 0.1, 0.1), color = color.red, opacity = 1)
-
     # generate the connecting rod
-    rod = cylinder(pos=vector(0, 3.05+heightadjust, backboard.pos.z+0.05), axis = vector(0, 0, 0.18), radius = 0.03, color=color.red, opacity =1)
-    pillar = cylinder(pos=vector(0, heightadjust, backboard.pos.z-0.2), axis = vector(0, 3.3,0), radius = 0.2, color = color.cyan)
+    rod = cylinder(pos=vector(0, hoopheight+heightadjust, backboard.pos.z+backboard.size.z/2), axis = vector(0, 0, 0.18), radius = 0.03, color=color.red, opacity =1)
     
+    # generate the pillar
+    pillar = cylinder(pos=vector(0, heightadjust, backboard.pos.z), axis = vector(0, 3.3,0), radius = 0.2, color = color.cyan)
+    pillar.pos.z -= pillar.radius +backboard.size.z/2
+    
+    # generate the paint area
+    boardthickness = backboard.size.z/2
+    paintlength = 5.8
+    paintwidth = 4.57
+    paintleft = box(pos = vector(paintwidth/2, heightadjust,z_boarder+paintlength/2+boardposition/2), size = vector(0.1, 0.1, paintlength+boardposition), color = color.red, opacity = 1)
+    paintright = box(pos = vector(-paintwidth/2, heightadjust,z_boarder+paintlength/2+boardposition/2), size = vector(0.1, 0.1, paintlength+boardposition), color = color.red, opacity = 1)
+    freethrowline = box(pos = vector(0, heightadjust,paintlength+z_boarder+boardposition), size = vector(paintwidth, 0.1, 0.1), color = color.red, opacity = 1)
+
+  
     # define the distance between the ball and the hoop
-    dist_2_point = 4.57 
-    dist_3_point = 7.24
+    # dist_2_point = 4.57  # width
+    # dist_3_point = 7.24 # length
     
     # generate the basketball
+    shootheight = 1.9
     ball_radius = 0.12
-    basketball = sphere(pos=vector(0, player_height+heightadjust, dist_2_point-ball_radius), radius=ball_radius, color=color.orange, make_trail=False, trail_color=color.white)
+    basketball = sphere(pos=vector(0, shootheight+heightadjust,  freethrowline.pos.z), radius=ball_radius, color=color.orange, make_trail=False, trail_color=color.white)
     
     def set_shoot_position(self, level): # level 0: 2-point shot, level 1: 3-point shot
-        dist = self.dist_2_point if level == 0 else self.dist_3_point
-        self.basketball.pos = vector(0, self.player_height+self.heightadjust, dist-self.ball_radius)
+        dist = self.paintwidth if level == 0 else self.paintlength
+        self.basketball.pos = vector(0, self.shootheight+self.heightadjust, dist-self.ball_radius)
     
     def shoot(self, ax = 0, ay = 0, az = 0):
         # initial position of the ball
@@ -46,7 +59,7 @@ class basketball():
             time += dt
             if time > maxtime:
                 time = 0
-                self.basketball.pos = vector(0, self.player_height+self.heightadjust, self.dist_2_point-self.ball_radius)
+                self.basketball.pos = vector(0, self.shootheight+self.heightadjust, self.paintwidth-self.ball_radius)
                 return False
             
             rate(800)  # 1000 frames per second
@@ -75,7 +88,7 @@ class basketball():
 
             
             # if the ball is bumped into the backboard, bounce
-            if(self.basketball.pos.z <= self.backboard.pos.z+0.05+self.basketball.radius) and (self.basketball.pos.z >= self.backboard.pos.z - 0.05-self.basketball.radius) and (self.basketball.pos.y <= self.backboard.pos.y+self.backboard.size.y/2) and (self.basketball.pos.y >= self.backboard.pos.y - self.backboard.size.y/2) and self.basketball.velocity.z < 0:
+            if(self.basketball.pos.z <= self.backboard.pos.z+self.boardthickness+self.basketball.radius) and (self.basketball.pos.z >= self.backboard.pos.z -self.boardthickness-self.basketball.radius) and (self.basketball.pos.y <= self.backboard.pos.y+self.backboard.size.y/2) and (self.basketball.pos.y >= self.backboard.pos.y - self.backboard.size.y/2) and self.basketball.velocity.z < 0:
                 self.basketball.velocity.z *= -0.3
                 self.basketball.velocity.y *= 0.3
             
